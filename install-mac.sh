b6eb147af72f5147fcba9603ce7bb4eef5842361
pranavgadre <pranav.vgadre@gmail.com>
2026-02-09 21:23:39 -0800

    Update cmake version

--- install-mac.sh
+++ install-mac.sh
@@ -8,7 +8,8 @@ mkdir build
 export PKG_CONFIG_PATH=/usr/local/opt/openssl/lib/pkgconfig 
 cd build
 OPENSSL_VERSION=`brew list --versions openssl | cut -d' ' -f2`
-cmake -DOPENSSL_ROOT_DIR=$(brew --cellar openssl)/$OPENSSL_VERSION -DOPENSSL_LIBRARIES=$(brew --cellar openssl)/$OPENSSL_VERSION/lib ..
+# Ensure CMake policy compatibility for older project files
+cmake -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DOPENSSL_ROOT_DIR=$(brew --cellar openssl)/$OPENSSL_VERSION -DOPENSSL_LIBRARIES=$(brew --cellar openssl)/$OPENSSL_VERSION/lib ..
 make 
 sudo make install
 cd ..
